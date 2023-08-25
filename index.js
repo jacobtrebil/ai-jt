979a38dc4714734976e2cc64a5e4b9c07f327606
Jacob Trebil <jacobtrebil@yahoo.com>
2023-08-25 15:44:33 -0700

    added openai

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 const express = require('express')
 const app = express()
 const PORT = 4000
+const ai = require('.//api/completion/route');
 
 app.get('/', (req, res) => {
+  console.log('Welcome to the Express API');
   res.status(200).json('Welcome, your app is working well');
 })
+
+app.use('/api/ai', ai);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
